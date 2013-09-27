44e2663c3ba3506acc42e04a76e2f2499a0ba85f
mikeatsmalltalk <michael@smalltalkpublishing.com>
2013-09-27 02:07:40 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LatinGrammyApp::Application.config.secret_token = '7ac78905f66d8c81138042a7eb6ad30502a0ec69fd85ac78b207013aaec4181edbf95933860845e15597e4ee750c4be72632270a4fd3299374e62adcb339faaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LatinGrammyApp::Application.config.session_store :cookie_store, key: '_latin_grammy_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LatinGrammyApp::Application.config.session_store :active_record_store
